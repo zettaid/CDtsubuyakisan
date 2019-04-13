945a1a0c6f40a4d3001f1f2c83276593ae776cfe
Apple <apple@Apple-no-MacBook-Air.local>
2019-04-13 13:16:10 +0900

    define user actions and create  user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+	def top
+	
+	end
+
+	def show
+
+	end
+
+	def index
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def delete
+
+	end
+
 end
