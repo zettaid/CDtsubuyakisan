8a39b264c1e70c73ca628ba2b0a805ca0edcb270
福地友望子 <fukuchiyumiko@YFMacBook-Air.local>
2019-04-15 10:00:11 +0900

    [add CD登録画面 モーダルウィンドウ表示]

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -1,7 +1,11 @@
 class Cd < ApplicationRecord
-	scope :search, (->(word) { where('artist.name LIKE ? OR cd_name LIKE ?',
-                                   "%#{sanitize_sql_like(word)}%",
-                                   "%#{sanitize_sql_like(word)}%") })
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      Cd.where(['name LIKE ?', "%#{search}%"])
+    else
+      Cd.all #全て表示。
+    end
+  end
 	has_many :artists
 	has_many :labels
 	has_many :genres
