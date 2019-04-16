77a64576051a5dd7bb8248cf1686394ed529c027
yusa0730 <isizawa5226@gmail.com>
2019-04-16 19:14:51 +0900

    deleate log&tmp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	helper_method :current_cart
+
+	def current_cart
+		if session[:cart_id]
+			@cart = Cart.find(session[:cart_id])
+		else
+			@cart = Cart.create
+			session[:cart_id] = @cart.id
+		end
+	end
 end
