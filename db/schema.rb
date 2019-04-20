1ae9da709f428c0920ae75f6f96ba978ff08d1e4
no3319@gmail.com <no3319@gmail.com>
2019-04-20 13:17:32 +0900

    modify migration

--- db/schema.rb
+++ db/schema.rb
@@ -52,4 +52,53 @@ ActiveRecord::Schema.define(version: 2019_04_18_011744) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "image_id"
+    t.text "text"
+    t.integer "user_id"
+    t.integer "cd_id"
+    t.boolean "deleated"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.string "master"
+    t.string "post_number"
+    t.text "address"
+    t.string "phone_number"
+    t.text "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "email", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "post_number", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.text "address", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "deleated", default: false, null: false
+    t.boolean "admin", default: false, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
